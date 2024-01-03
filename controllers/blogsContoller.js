ae8b59259935c7f259bbbf09ed1caee3bf61db94
CristianTalaiguaNRA <cristian.talaigua@numrot.com>
2024-01-03 09:21:53 -0500

    Feat: finished part 4.10 Blog list testing, step 3

--- controllers/blogsContoller.js
+++ controllers/blogsContoller.js
@@ -1,17 +1,54 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blogModel');
 
-blogsRouter.get('/', async (request, response) => {
+blogsRouter.get('/', async (request, response,next) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
 
-blogsRouter.post('/', (request, response) => {
-  const blog = new Blog(request.body);
+blogsRouter.get('/:id', async (request,response,next)=>{
+  const blog = await Blog.findById(request.params.id)
+  if(blog){
+    response.json(blog)
+  } else {
+    response.status(404).end();
+  }
+});
+
+blogsRouter.post('/', (request, response,next) => {
+  const body = request.body;
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  });
 
   blog.save().then((result) => {
     response.status(201).json(result);
   });
 });
+
+blogsRouter.delete('/:id', async (request, response,next) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response,next)=>{
+  const body = request.body;
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+  response.json(updatedBlog);
+})
 
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,55 +1,74 @@
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const app = require('../app')
-const api = supertest(app)
-const Blog = require('../models/blogModel')
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const api = supertest(app);
+const Blog = require('../models/blogModel');
 const initialBlogs = [
   {
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
     likes: 7,
   },
   {
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
   },
-]
+];
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
-  await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
-  await blogObject.save()
+  await Blog.deleteMany({});
+  let blogObject = new Blog(initialBlogs[0]);
+  await blogObject.save();
+  blogObject = new Blog(initialBlogs[1]);
+  await blogObject.save();
+});
+
+describe('all blogs are returned', () => {
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs');
+
+    console.log(response.body);
+    const titles = response.body.map((r) => r.title);
+    expect(titles).toContain('React patterns');
+  });
+});
+
+describe('unique identifier of blog posts is called id', () => {
+  test('unique identifier name id', async () => {
+    const response = await api.get('/api/blogs');
+
+    console.log(response.body);
+    const id = response.body.map((r) => r.id);
+    expect(id).toBeDefined();
+  });
+});
+
+describe('a valid note can be added', ()=> {
+  test('a valid note can be added',async ()=>{
+    const newBlog = {
+      title: 'Un día cualquiera',
+      author: 'Anonimo',
+      url: 'www.undíacualquiera.com',
+      likes: 12
+    }
+
+    await api.post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(blog => blog.title)
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(titles).toContain('Un día cualquiera')
+  })
 })
 
-test('all blogs are returned', async () => {
-    const response = await api.get('/api/blogs')
-
-    expect(response.body).toHaveLength(initialBlogs.length)
-  })
-  
-  test('a specific blog is within the returned blogs', async () => {
-    const response = await api.get('/api/blogs')
-
-    console.log(response.body)
-    const titles = response.body.map(r => r.title)
-    expect(titles).toContain(
-      'React patterns'
-    )
-  })
-
-  test('unique identifier of blog posts is called id', async () => {
-    const response = await api.get('/api/blogs')
-
-    console.log(response.body)
-    const id = response.body.map(r => r.id)
-    expect(id).toBeDefined()
-  })
-  
-  afterAll(() => {
-    mongoose.connection.close()
-  })
+afterAll(() => {
+  mongoose.connection.close();
+});
